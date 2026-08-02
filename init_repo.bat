088f0371ecd60cd86530bb58378b9feab0ef3a45
raulcontreraso-bit <raulcontreraso@gmail.com>
2026-08-02 19:53:23 +0200

    Initial commit: Add project folder structure and README files

---
+++ init_repo.bat
@@ -0,0 +1,8 @@
+git branch -M main
+git remote add origin https://github.com/raulcontreraso-bit/cs-engineer-workspace.git
+git add .
+git commit -m "Add project folder structure and initial README files"
+git push -u origin main
+
+echo Git successfully!
+pause
